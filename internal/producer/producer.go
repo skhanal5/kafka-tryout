97dd75842caf8456e7202375e181e04b8f82d21f
Subodh <skhanal5@terpmail.umd.edu>
2025-05-18 17:25:47 -0400

    Add initial producer client implementation

---
+++ internal/producer/producer.go
@@ -0,0 +1,47 @@
+package producer
+
+import (
+	"context"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Producer interface {
+	
+}
+
+
+type ProducerClient struct {
+	writer *kafka.Writer
+}
+
+func (p *ProducerClient) WriteMessage(ctx context.Context, message string) {
+	msg := kafka.Message{
+		Value: []byte(message),
+	}
+	err := p.writer.WriteMessages(ctx, msg)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+}
+
+func (p *ProducerClient) Shutdown() {
+	err := p.writer.Close()
+	if err != nil {
+		log.Fatal("failed to close client:", err)
+	}
+}
+
+func NewProducer(port string, topic string) *ProducerClient {
+	w := &kafka.Writer{
+		Addr:     kafka.TCP(port),
+		Topic:   topic,
+		Balancer: &kafka.LeastBytes{},
+	}
+	return &ProducerClient{
+		writer: w,
+	}
+	
+}
